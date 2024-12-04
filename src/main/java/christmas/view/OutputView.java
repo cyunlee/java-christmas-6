35d06ef96ad4aa4a94063028a2e33774050c8b7b
cyunlee <siyunkr@gmail.com>
2024-12-04 21:14:56 +0900

    feat: OutputVeiw의 기본 메서드 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,8 +3,20 @@ package christmas.view;
 public class OutputView {
     private final static String ERROR = "[ERROR] ";
 
-    public void printPrompt() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    public void printWelcomePrompt() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public void printExpectingDatePrompt() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+    }
+
+    public void printOrderPrompt() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+    }
+
+    public void printEventBenefit(int month, int date) {
+        System.out.printf("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!%n", month, date);
     }
 
     public void printError(String message) {
